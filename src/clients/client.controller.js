9464a44442cd9177c08679324a08ad4faa695637
IanAlfaroSantos <ialfasa2020@gmail.com>
2025-02-26 15:34:36 -0600

    CRUD Client added complete

--- src/clients/client.controller.js
+++ src/clients/client.controller.js
@@ -1,5 +1,4 @@
 import Client from "./client.model.js";
-import { hash, verify } from "argon2";
 import { request, response } from "express";
 
 export const saveClient = async (req, res) => {
@@ -32,6 +31,173 @@ export const saveClient = async (req, res) => {
         res.status(500).json({
             success: false,
             message: "Error saving client"
+        });
+    }
+}
+
+export const getClients = async (req = request, res = response) => {
+    try {
+
+        const { limite = 10, desde = 0 } = req.body;
+        const query = { estado: true };
+
+        if (req.admin.role !== "ADMIN") {
+            return res.status(400).json({
+                success: false,
+                message: "You do not have permissions to get clients"
+            });
+        }
+
+        const [total, clients] = await Promise.all([
+            Client.countDocuments(query),
+            Client.find(query)
+            .skip(desde)
+            .limit(parseInt(limite))
+        ]);
+
+        res.status(200).json({
+            success: true,
+            total,
+            clients
+        });
+        
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Error getting clients"
+        });
+    }
+}
+
+export const getClientById = async (req, res) => {
+    try {
+
+        const { id } = req.params;
+
+        if (req.admin.role !== "ADMIN") {
+            return res.status(400).json({
+                success: false,
+                message: "You do not have permissions to search clients"
+            });
+        }
+
+        const client = await Client.findById(id);
+
+        if (client.estado === false) {
+            return res.status(400).json({
+                success: false,
+                message: "Client is disabled"
+            });
+        }
+        
+        if (!client) {
+            return res.status(400).json({
+                success: false,
+                message: "Client not found"
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            client
+        });
+        
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Error getting client by ID"
+        });
+    }
+}
+
+export const updateClient = async (req, res = response) => {
+    try {
+
+        const { id } = req.params;
+        const { _id, email, ...data } = req.body;
+
+        const client = await Client.findById(id);
+        
+        if (client.estado === false) {
+            return res.status(400).json({
+                success: false,
+                message: "Client is disabled"
+            });
+        }
+        
+        if (!client) {
+            return res.status(400).json({
+                success: false,
+                message: "Client not found"
+            });
+        }
+
+        if (req.admin.role !== "ADMIN") {
+            return res.status(400).json({
+                success: false,
+                message: "You do not have permissions to update clients"
+            });
+        }
+
+        const updatedClient = await Client.findByIdAndUpdate(id, data, { new: true });
+
+        res.status(200).json({
+            success: true,
+            message: "Client updated successfully",
+            updatedClient
+        });
+        
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Error updating client"
+        });
+    }
+}
+
+export const deleteClient = async (req, res = response) => {
+    try {
+
+        const { id } = req.params;
+        
+        const authenticatedClient = req.client;
+
+        const client = await Client.findById(id);
+
+        if (client.estado === false) {
+            return res.status(400).json({
+                success: false,
+                message: "The client is already disabled"
+            });
+        }
+
+        if (!client) {
+            return res.status(400).json({
+                success: false,
+                message: "Client not found"
+            });
+        }
+        
+        if (req.admin.role !== "ADMIN") {
+            return res.status(400).json({
+                success: false,
+                message: "You do not have permissions to delete clients"
+            });
+        }
+
+        const deleteClient = await Client.findByIdAndUpdate(id, { estado: false }, { new: true });
+
+        res.status(200).json({
+            success: true,
+            message: "Client deleted successfully",
+            deleteClient,
+            authenticatedClient
+        });
+        
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Error deleting client"
         });
     }
 }
--- src/clients/client.routes.js
+++ src/clients/client.routes.js
@@ -3,7 +3,7 @@ import { check } from "express-validator";
 import { existenteClienById } from "../helpers/db-validator.js";
 import { validarCampos } from "../middlewares/validar-campos.js";
 import { validarJWT } from "../middlewares/validar-jwt.js";
-import { saveClient } from "./client.controller.js";
+import { saveClient, getClients, getClientById, updateClient, deleteClient } from "./client.controller.js";
 import { validatorEmailClient } from "../middlewares/validator.js";
 
 const router = Router();
@@ -16,6 +16,45 @@ router.post(
         validarCampos
     ],
     saveClient
+);
+
+router.get(
+    '/',
+    validarJWT,
+    getClients
+);
+
+router.get(
+    '/:id',
+    [
+        validarJWT,
+        check('id', 'Invalid ID').isMongoId(),
+        check('id').custom(existenteClienById),
+        validarCampos
+    ],
+    getClientById
+);
+
+router.put(
+    '/:id',
+    [
+        validarJWT,
+        check('id', 'Invalid ID').isMongoId(),
+        check('id').custom(existenteClienById),
+        validarCampos
+    ],
+    updateClient
+);
+
+router.delete(
+    '/:id',
+    [
+        validarJWT,
+        check('id', 'Invalid ID').isMongoId(),
+        check('id').custom(existenteClienById),
+        validarCampos
+    ],
+    deleteClient
 );
 
 export default router;
