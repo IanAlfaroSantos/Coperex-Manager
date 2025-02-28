6d1a8c04d2c5d9eca2defb7ee35ff1029c4e8654
IanAlfaroSantos <ialfasa2020@gmail.com>
2025-02-27 21:39:06 -0600

    CRUD Company added complete

--- src/companies/company.controller.js
+++ src/companies/company.controller.js
@@ -235,14 +235,19 @@ export const generateReport = async (req, res) => {
         });
 
         
+        const buffer = await book.xlsx.writeBuffer();
         res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-        res.setHeader('Content-Disposition', 'attachment; filename="Report Companies.xlsx"');
-        
+        res.setHeader('Content-Disposition', 'attachment; filename=Report_Companies.xlsx');
+        res.send(buffer);
+                
         await book.xlsx.write(res);
         
         res.end();
+
+        return;
     
     } catch (error) {
+        console.log(error);
         res.status(500).json({
             success: false,
             message: "Error generating the report"
